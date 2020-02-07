6d3eb974dd9f18f026342bbca94134d661db1e4e
foryourselfand <foryourselfand@gmail.com>
2020-02-07 11:43:48 +0300

    more testing timezone

--- rest_api_demo/testing.py
+++ rest_api_demo/testing.py
@@ -6,16 +6,17 @@ from pytz import timezone
 def main():
     now = datetime.datetime.now()
     
-    moscow_timezone = timezone('Europe/Moscow')
-    eastern_timezone = timezone('US/Eastern')
+    first_timezone = timezone('Europe/Moscow')
+    second_timezone = timezone('Asia/Yekaterinburg')
+    # second_timezone = timezone('US/Eastern')
     
-    moscow_localize = moscow_timezone.localize(now)
-    eastern_localize = eastern_timezone.localize(now)
+    first_localize = first_timezone.localize(now)
+    second_localize = second_timezone.localize(now)
     
-    print(f'{moscow_localize=}')
-    print(f'{eastern_localize=}')
+    print(f'{first_localize=}')
+    print(f'{second_localize=}')
 
-    time_diff = int((moscow_localize - eastern_localize).total_seconds() / 3600)
+    time_diff = int((first_localize - second_localize).total_seconds() / 3600)
     print(f'{time_diff=}')
     
     
